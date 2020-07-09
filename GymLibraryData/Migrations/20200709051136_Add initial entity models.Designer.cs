45fdbb9f6b5240b9e393ecf6ef7d75b832a873ef
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-07-08 19:12:09 -1000

    updated database with the new schema

--- GymLibraryData/Migrations/20200709051136_Add initial entity models.Designer.cs
+++ GymLibraryData/Migrations/20200709051136_Add initial entity models.Designer.cs
@@ -4,14 +4,16 @@ using GymLibraryData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GymLibraryData.Migrations
 {
     [DbContext(typeof(GymContext))]
-    partial class GymContextModelSnapshot : ModelSnapshot
+    [Migration("20200709051136_Add initial entity models")]
+    partial class Addinitialentitymodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
