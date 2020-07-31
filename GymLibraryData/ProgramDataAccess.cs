98a62c7fa5553ab2141733547d781ff129bc46ff
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-07-31 06:11:18 -0300

    Trying to make the programs page, need to fix some bugs/errors

---
+++ GymLibraryData/ProgramDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using GymLibraryData.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GymLibraryData
+{
+    public class ProgramDataAccess
+    {
+        private SqlDbContext db;
+        
+        public ProgramDataAccess(SqlDbContext db)
+        {
+            this.db = db;
+        }
+        public List<Program> SelectAll()
+        {
+            return db.programs.ToList();
+        }
+    }
+}
