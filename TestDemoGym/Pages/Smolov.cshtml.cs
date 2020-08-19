c8cadcffeb404e15523a207584244fa918bda9cc
Vladislav Smirnov <55094161+Pr0Vlad@users.noreply.github.com>
2020-08-19 14:47:11 -0400

    Getting client side date on server side works..now need to store and then show program on the client side

--- TestDemoGym/Pages/Smolov.cshtml.cs
+++ TestDemoGym/Pages/Smolov.cshtml.cs
@@ -9,9 +9,18 @@ namespace TestDemoGym.Pages
 {
     public class SmolovModel : PageModel
     {
+        public string CurrentMax { get; set; }
         public void OnGet()
         {
 
+        }
+        public void OnPost()
+        {
+            //this takes the input and prints in console
+            //need to store it and access it in the client side programming for the program
+            CurrentMax = Request.Form["currentmax"];
+            
+            Console.WriteLine(CurrentMax);
         }
     }
 }
